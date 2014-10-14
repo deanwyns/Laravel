453a1c710652638f542795b24b47171bb5969d2c
Dean Wyns <dean.wyns@outlook.com>
2014-10-14 18:20:57 +0200

    Model binding uitleg toegevoegd. User table migration toegevoegd (incompleet, maar met uitleg).

--- app/bindings.php
+++ app/bindings.php
@@ -1,1 +1,58 @@
 <?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Binders
+|--------------------------------------------------------------------------
+| Implement your model bindings in here. There is no default "spot" in
+| Laravel to put these, so I made one over here. :-)
+|
+*/
+
+/**
+ * Voorbeeld
+ *
+ * In je route (routes.php) kun je aan route model binding doen.
+ * Wat dit inhoudt, is dat je bijvoorbeeld de gebruiker automatisch
+ * kan laten opzoeken met de naam "Jan" bij het surfen naar
+ * www.site.be/user/jan
+ *
+ * De "action" waar deze route naar leidt, krijgt dan als parameter
+ * User $user
+ * In $user wordt automatisch de gebruiker met voornaam "Jan" geïnjecteerd.
+ *
+ * Er zijn ook standaard model binders die je niet moet implementeren.
+ * Die werken sowieso met de id's. Dus bijvoorbeeld
+ * www.site.be/user/5
+ * De gebruiker met id 5 wordt dan geïnjecteerd.
+ * Dit doe je als volgt:
+ * 
+ * Route::model('user', 'User');
+ *
+ * Je kan ook een closure als 3de parameter toevoegen die moet uitgevoerd worden
+ * als de bijhorende model niet is gevonden:
+ * 
+ * Route::model('user', 'User', function() {
+ *     throw new NotFoundHttpException;
+ * });
+ *
+ * Als je nu per sé zoals eerst aangehaald, de gebruikers met de voornaam
+ * wilt opzoeken, moet je een custom model binder maken.
+ * Dit doe ik in het voorbeeld hieronder.
+ * Wel niet vergeten de route aan te passen!
+ * Route::get('/user/{user}', ...); <--- {user} stelt dan je model voor
+ *
+ * Makkelijk niet? Zoveel hokus pokus! Wauw!
+ * 
+ */
+Route::bind('user', function($value, $route) { // user is wat er tussen {} staat!
+	// value bevat hier in dit voorbeeld de voornaam
+	$user = User::where('firstname', '=', $value)->first();
+	if(is_null($user)) {
+		// Als de gebruiker met value (bv Jan) als firstname niet gevonden is, throw je een exception
+		throw new NotFoundHttpException;
+	}
+
+	// Wel gevonden? Return de gebruiker die geïnjecteerd moet worden in je functie!
+	return $user;
+});
---
+++ app/database/migrations/2014_10_14_160710_create_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email');
+			$table->string('password');
+
+			// Als de user iets "heeft", dan moet je manueel de vreemde sleutels toevoegen.
+			// De rest doet Laravel wél zelf! Dat is toch zo als je je houdt aan de normale
+			// benaming van die vreemde sleutels.
+			// Als een user een "Book" (model Book) heeft, dan schrijf je "book_id".
+			// Bv. $table->integer('book_id')->unsigned();
+			// Laravel gaat dus uit van "modelname_id".
+			// 
+			// Vreemde sleutels zijn trouwens unsigned, want id is best niet onder nul!
+			// Je kunt ook $table->unsignedInteger('bla') doen.
+
+			// Wordt door Laravel gebruikt om gebruikers te onthouden!
+			$table->string('remember_token');
+
+			// Maakt een created_at en updated_at kolom aan. Die worden automatisch geüpdatet bij een save() van het Model
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
