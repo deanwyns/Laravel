e220a59c68fa5ffdc5fd6bd9acf5d86e51b3770c
Dean Wyns <dean.wyns.t9335@student.hogent.be>
2014-12-14 20:40:01 +0100

    Oeps, komt ervan als je te vermoeid bent :P

--- app/models/VacationTransformer.php
+++ app/models/VacationTransformer.php
@@ -3,7 +3,6 @@ class VacationTransformer extends League\Fractal\TransformerAbstract {
 
     public function transform(Vacation $vacation) {
         $currentParticipants = $vacation->registrations->count();
-        //$likes = sizeof($vacation->likes);
         $likes = $vacation->likes->count();
         $categoryPhoto = URL::route('category.photo', ['category' => $vacation->category_id]);
 
@@ -17,7 +16,7 @@ class VacationTransformer extends League\Fractal\TransformerAbstract {
             'age_to' => $vacation->age_to,
             'transportation' => $vacation->transportation,
             'max_participants' => $vacation->max_participants,
-            'current_participants' => $current_participants,
+            'current_participants' => $currentParticipants,
             'base_cost' => $vacation->base_cost,
             'one_bm_member_cost' => $vacation->one_bm_member_cost,
             'two_bm_member_cost' => $vacation->two_bm_member_cost,
